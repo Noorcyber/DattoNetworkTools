1588b880eb58a887283fb6c5d3ac22a591c7ed48
DattoCorn <139921759+DattoCorn@users.noreply.github.com>
2023-07-18 17:11:07 -0400

    Update SimpAnalyzer.sh

--- SimpAnalyzer.sh
+++ SimpAnalyzer.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# Specify the URL of the MAC-to-vendor JSON file
-MAC_VENDOR_URL="https://raw.githubusercontent.com/DattoCorn/DattoNetworkTools/main/VendorList.json"
+# Specify the URL of the MAC-to-vendor TXT file
+MAC_VENDOR_URL="https://raw.githubusercontent.com/DattoCorn/DattoNetworkTools/main/VendorList.txt"
 
-# Retrieve the MAC-to-vendor JSON file
-MAC_VENDOR_FILE=$(curl -s "$MAC_VENDOR_URL")
+# Retrieve the MAC-to-vendor mapping from the TXT file
+curl -s "$MAC_VENDOR_URL" -o mac_vendors.txt
+
+# Specify the path to the MAC-to-vendor TXT file
+MAC_VENDOR_FILE="mac_vendors.txt"
+
+# Retrieve the ARP table
+ARP_TABLE=$(cat /proc/net/arp)
 
 # Print the custom column headers
-printf "----------------------Simp Tool V1------------------------\n"
+printf "----------------------Simp Tool V1.3------------------------\n"
 printf "%-15s %-17s %-12s %-15s\n" "IP Address" "HW Address" "Link" "Vendor"
 printf "---------------------------------------------------------\n"
 
@@ -16,19 +22,16 @@ get_vendor_name() {
   local mac_address="$1"
   local vendor_name="Unknown"
 
-  # Lookup the vendor in the MAC-to-vendor JSON file
-  if [ -n "$MAC_VENDOR_FILE" ]; then
-    vendor_name=$(echo "$MAC_VENDOR_FILE" | jq -r ".[\"$mac_address\"]")
-    if [ "$vendor_name" = "null" ]; then
+  # Lookup the vendor in the MAC-to-vendor TXT file
+  if [ -f "$MAC_VENDOR_FILE" ]; then
+    vendor_name=$(grep -i "$mac_address" "$MAC_VENDOR_FILE" | awk '{print $2}')
+    if [ -z "$vendor_name" ]; then
       vendor_name="Unknown"
     fi
   fi
 
   echo "$vendor_name"
 }
-
-# Retrieve the ARP table
-ARP_TABLE=$(cat /proc/net/arp)
 
 # Iterate over each line in the ARP table
 echo "$ARP_TABLE" | while read -r line
@@ -43,3 +46,6 @@ do
   # Output the custom formatted information
   printf "%-15s %-17s %-12s %-15s\n" "$IP_ADDRESS" "$MAC_ADDRESS" "$DEVICE" "$VENDOR_NAME"
 done
+
+# Remove the temporary MAC-to-vendor TXT file
+rm -f "$MAC_VENDOR_FILE"
