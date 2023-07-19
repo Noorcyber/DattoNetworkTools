94cf4c5ee9b047579c3a714c2c5581ef9e45cfa5
DattoCorn <139921759+DattoCorn@users.noreply.github.com>
2023-07-19 09:34:33 -0400

    Update SimpAnalyzer.sh

--- SimpAnalyzer.sh
+++ SimpAnalyzer.sh
@@ -7,7 +7,7 @@ MAC_NAME1="Datto Devices"
 MAC_NAME2="Vendor 2 Devices"
 
 # Specify the MAC address patterns for Tuya Devices
-TUYA_MAC_PATTERNS=("10:5a:17" "10:d5:61" "18:69:d8" "1c:90:ff" "38:1f:8d" "50:8a:06" "68:57:2d" "70:89:76" "7c:f6:66" "84:e3:42" "a0:92:08" "cc:8c:bf" "d4:a6:51" "d8:1f:12" "fc:67:1f")
+TUYA_MAC_PATTERNS="10:5a:17|10:d5:61|18:69:d8|1c:90:ff|38:1f:8d|50:8a:06|68:57:2d|70:89:76|7c:f6:66|84:e3:42|a0:92:08|cc:8c:bf|d4:a6:51|d8:1f:12|fc:67:1f"
 TUYA_MAC_NAME="Tuya Devices"
 
 # Retrieve the ARP table
@@ -32,18 +32,15 @@ do
   MATCHED="N/A"
   VENDOR_NAME="Unknown"
   
-  if [ "$MAC_PREFIX" = "$MAC_PATTERN1" ] || [ "$MAC_PREFIX" = "$MAC_PATTERN2" ]; then
+  if [ "$(echo "$MAC_PREFIX" | grep -E "^($MAC_PATTERN1|$MAC_PATTERN2)$")" ]; then
     MATCHED="Matched"
     VENDOR_NAME="$MAC_NAME1"
   fi
   
-  for TUYA_MAC_PATTERN in "${TUYA_MAC_PATTERNS[@]}"; do
-    if [ "$MAC_PREFIX" = "$TUYA_MAC_PATTERN" ]; then
-      MATCHED="Matched"
-      VENDOR_NAME="$TUYA_MAC_NAME"
-      break
-    fi
-  done
+  if [ "$(echo "$MAC_PREFIX" | grep -E "^($TUYA_MAC_PATTERNS)$")" ]; then
+    MATCHED="Matched"
+    VENDOR_NAME="$TUYA_MAC_NAME"
+  fi
   
   # Output the custom formatted information for matched MAC address prefixes
   printf "%-15s %-17s %-12s %-15s\n" "$IP_ADDRESS" "$MAC_ADDRESS" "$DEVICE" "$VENDOR_NAME"
