10af9c464ea076bece04083f71f6d068b6c20e57
DattoCorn <139921759+DattoCorn@users.noreply.github.com>
2023-07-19 09:15:57 -0400

    Update SimpAnalyzer.sh

--- SimpAnalyzer.sh
+++ SimpAnalyzer.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-# Specify the URL of the MAC-to-vendor TXT file
-MAC_VENDOR_URL="/tmp/VendorList.txt"
-
-# Retrieve the MAC-to-vendor mapping from the TXT file
-curl -s "$MAC_VENDOR_URL" -o mac_vendors.txt
-
-# Specify the path to the MAC-to-vendor TXT file
-MAC_VENDOR_FILE="mac_vendors.txt"
+# Define the MAC-to-vendor mapping
+declare -A MAC_VENDORS=(
+  ["10:5a:17"]="Tuya Devices"
+  ["10:d5:61"]="Tuya Devices"
+  ["18:69:d8"]="Tuya Devices"
+  ["1c:90:ff"]="Tuya Devices"
+  ["38:1f:8d"]="Tuya Devices"
+  ["50:8a:06"]="Tuya Devices"
+  ["68:57:2d"]="Tuya Devices"
+  ["70:89:76"]="Tuya Devices"
+  ["7c:f6:66"]="Tuya Devices"
+  ["84:e3:42"]="Tuya Devices"
+  ["a0:92:08"]="Tuya Devices"
+  ["cc:8c:bf"]="Tuya Devices"
+  ["d4:a6:51"]="Tuya Devices"
+  ["d8:1f:12"]="Tuya Devices"
+  ["fc:67:1f"]="Tuya Devices"
+  ["f8:d9:b8"]="Datto Devices"
+  ["ac:86:74"]="Datto Devices"
+)
 
 # Retrieve the ARP table
 ARP_TABLE=$(cat /proc/net/arp)
@@ -20,16 +32,10 @@ printf "---------------------------------------------------------\n"
 # Function to lookup the vendor name based on MAC address
 get_vendor_name() {
   local mac_address="$1"
-  local vendor_name="Unknown"
-
-  # Lookup the vendor in the MAC-to-vendor TXT file
-  if [ -f "$MAC_VENDOR_FILE" ]; then
-    vendor_name=$(grep -i "$mac_address" "$MAC_VENDOR_FILE" | awk '{print $2}')
-    if [ -z "$vendor_name" ]; then
-      vendor_name="Unknown"
-    fi
+  local vendor_name=${MAC_VENDORS[$mac_address]}
+  if [ -z "$vendor_name" ]; then
+    vendor_name="Unknown"
   fi
-
   echo "$vendor_name"
 }
 
@@ -46,6 +52,3 @@ do
   # Output the custom formatted information
   printf "%-15s %-17s %-12s %-15s\n" "$IP_ADDRESS" "$MAC_ADDRESS" "$DEVICE" "$VENDOR_NAME"
 done
-
-# Remove the temporary MAC-to-vendor TXT file
-rm -f "$MAC_VENDOR_FILE"
